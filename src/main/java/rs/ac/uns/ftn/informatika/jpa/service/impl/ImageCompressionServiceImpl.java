0e3b1a7b810f8a017b5c0a849739d92661ed9bbb
Biljana <biljanakatanic7@gmail.com>
2024-11-13 21:39:46 +0100

    Kompresija slike

---
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/service/impl/ImageCompressionServiceImpl.java
@@ -0,0 +1,44 @@
+package rs.ac.uns.ftn.informatika.jpa.service.impl;
+
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class ImageCompressionServiceImpl {
+    // Metod za kompresiju slike
+    public void compressImage(String imagePath) throws IOException {
+        File inputFile = new File(imagePath);
+        File outputFile = new File(imagePath.replace(".jpg", "_compressed.jpg")); // Kreira novu kompresovanu sliku
+
+        Thumbnails.of(inputFile)
+                .size(800, 600) // Željena veličina slike
+                .outputQuality(0.7) // Željeni kvalitet slike
+                .toFile(outputFile);
+    }
+
+    // Zakazivanje zadatka koji se pokreće svakog dana u ponoć
+    @Scheduled(cron = "0 0 0 * * ?")  // Pokreće se svakog dana u 00:00
+    public void compressOldImages() {
+        File directory = new File("uploads/images"); // Putanja do direktorijuma sa slikama
+        File[] files = directory.listFiles((dir, name) -> name.endsWith(".jpg"));
+
+        if (files != null) {
+            for (File file : files) {
+                long lastModified = file.lastModified();
+                long oneMonthAgo = new Date().getTime() - (30L * 24 * 60 * 60 * 1000); // Milisekunde za mesec dana
+
+                // Ako je slika starija od mesec dana, kompresuj je
+                if (lastModified < oneMonthAgo) {
+                    try {
+                        compressImage(file.getPath());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
